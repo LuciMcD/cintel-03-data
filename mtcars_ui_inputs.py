e2535aab85c7610cbd0a3af0c82e7184781064bf
Luci McDaniel <lucimcdaniel@gmail.com>
2023-07-14 12:17:45 -0500

    i don't know what I'm doing

--- mtcars_ui_inputs.py
+++ mtcars_ui_inputs.py
@@ -20,8 +20,19 @@ def get_mtcars_inputs():
             max=35,
             value=[10, 35],
         ),
-        ui.input_select("cyl: Number of cylinders", "Select Number of cylinders", {"A": "4", "B": "6", "C": "8"}),
         
+app_ui = ui.page_fluid(
+        ui.input_select("cyl: Number of cylinders", "Select Number", {"a": "4", "b": "6", "c": "8"}),
+        ui.output_text_verbatim("txt"),
+)
+def server(input, output, session):
+        @output
+        @render.text
+        def txt():
+            return f'cyl: "{input.x()}"'
+
+
+app = App(app_ui, server, debug=True)
         
          ui.tags.hr(),
          ui.tags.section(
